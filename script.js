f4823776c4d7f1ce4a950e41ace2a8123767f4a0
tracey <tracey.d.huang@gmail.com>
2022-08-19 01:40:42 +1000

    Re-tyle drop down menu

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ document.addEventListener('click', e => {
 
     //if we are clicking on button and link, close or open 
     let currentDropdown 
+    let button = document.querySelector("[data-dropdown-button]");
     if (isDropdownButton && e.target.closest("[data-dropdown]") != null) {
         currentDropdown = e.target.closest("[data-dropdown]") 
         currentDropdown.classList.toggle('active');
+        button.classList.toggle('active');
     }
 
     //close other dropdowns 
     document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
         if (dropdown === currentDropdown) return
         dropdown.classList.remove('active')
+        button.classList.remove('active');
     })
 })
